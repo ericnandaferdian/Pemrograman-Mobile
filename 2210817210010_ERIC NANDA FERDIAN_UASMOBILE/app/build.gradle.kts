e84e0fd5e63c464327a09129285bea10b4e84d57
ericnandaferdian <2210817210010@mhs.ulm.ac.id>
2024-07-04 02:26:41 +0800

    w

---
+++ 2210817210010_ERIC NANDA FERDIAN_UASMOBILE/app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.unoth.cryptoapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.unoth.cryptotracker"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    val retrofit_version = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:$retrofit_version")
+
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.8")
+
+    val lifecycle_version = "2.7.0"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    annotationProcessor("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:$lifecycle_version")
+
+
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    kapt("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.5.0")
+    implementation("androidx.room:room-ktx:$room_version")
+
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ 2210817210010_ERIC NANDA FERDIAN_UASMOBILE/app/src/main/java/com/unoth/cryptoapp/CoinPriceListActivity.kt
@@ -0,0 +1,45 @@
+package com.unoth.cryptoapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.unoth.cryptoapp.adapters.CoinInfoAdapter
+import com.unoth.cryptoapp.databinding.ActivityCoinPriceListBinding
+import com.unoth.cryptoapp.pojo.CoinPriceInfo
+
+class CoinPriceListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCoinPriceListBinding
+    private lateinit var viewModel: CoinViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCoinPriceListBinding.inflate(layoutInflater)
+        val view = binding.root
+        enableEdgeToEdge()
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromsymbol
+                )
+                startActivity(intent)
+            }
+        }
+        binding.rvCoinPriceList.adapter = adapter
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
+        })
+    }
+}
