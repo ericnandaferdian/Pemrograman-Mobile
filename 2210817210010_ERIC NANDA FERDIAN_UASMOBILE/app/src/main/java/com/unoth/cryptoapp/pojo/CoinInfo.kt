1a46db12ce5be032aa987c56f0d241352d18e9c4
ericnandaferdian <2210817210010@mhs.ulm.ac.id>
2024-07-04 09:56:53 +0800

    Add files via upload

---
+++ 2210817210010_ERIC NANDA FERDIAN_UASMOBILE/app/src/main/java/com/unoth/cryptoapp/pojo/CoinInfo.kt
@@ -0,0 +1,10 @@
+package com.unoth.cryptoapp.pojo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CoinInfo(
+    @SerializedName("Name")
+    @Expose
+    val name: String? = null
+)
